e3f192dbdae8436b72ff5fb76c518520a1e07487
JackAttack612 <jetflyer1130@gmail.com>
2021-09-22 19:11:52 -0400

    Create twitter-bot.py

---
+++ Projects/twitter-bot.py
@@ -0,0 +1,100 @@
+import tweepy
+import time
+import pwinput
+
+auth = tweepy.OAuthHandler("QjT3WQnlSIsO4dexw9C0TaIP4", "CeqHI2EcKnAJkbWU3IcCiEArqG8xWZphzXKJIc1WoT5piuUnJj")
+auth.set_access_token("1156848387163119616-h87w8HtypPHyvTRYUZGyz6jT3EP4EZ", "QnhxN5pDZ01YFaTR94lVpPI1vSKDsje2CDLCMvJQ9INKU")
+
+api = tweepy.API(auth)
+
+try:
+    api.verify_credentials()
+    print("Authentication OK")
+
+except:
+    print("Error during authentication")
+
+api = tweepy.API(auth, wait_on_rate_limit=True,
+    wait_on_rate_limit_notify=True)
+
+def read():
+    timeline = api.home_timeline()
+    for tweet in timeline:
+        print(f"{tweet.user.name} said {tweet.text}")
+        time.sleep(3)
+        main()
+
+def Tweet():
+    tweet_input = input("\nWhat do you want to tweet: ")
+    api.update_status(tweet_input)
+    print("You tweeted: " + tweet_input)
+    main()
+
+def user_info():
+    try:
+        user_info_name = input("\nWhat user do you want to find: ")
+        user = api.get_user(user_info_name)
+
+        print("User details:")
+        print('Username: ' + user.name)
+        print('User Description: ' + user.description)
+        print('User Location: ' + user.location)
+
+        print("\nLast 20 Followers:")
+        for follower in user.followers():
+            print(follower.name)
+        main()
+    except:
+        print("\nCould not find user")
+        main()
+
+def like_mentioned():
+    tweets = api.mentions_timeline()
+    for tweet in tweets:
+        tweet.favorite()
+    main()
+
+def mentioned():
+    tweets = api.mentions_timeline()
+    for tweet in tweets:
+        print(f"\n{tweet.user.name} said {tweet.text}")
+    time.sleep(3)
+    main()
+        
+def main():
+    start = input("\nWhat do you want to do: ").lower()
+    if start == 'help':
+        print("tweet\nread\nuserinfo\nlikementioned\nmentioned")
+        main()
+    elif start == 'tweet':
+        confirmation = input("\nAre you sure you want to send a tweet (Yes or No): ").lower()
+        if confirmation == 'yes':
+            Tweet()
+        elif confirmation == 'no':
+            main()
+        else:
+            print("\nDid not receive a valid input")
+            main()
+    elif start == 'read':
+        read()
+    elif start == 'userinfo':
+        user_info()
+    elif start == 'likementioned':
+        like_mentioned()
+    elif start == 'mentioned':
+        mentioned()
+    else:
+        print("\nDid not receive a valid input")
+        main()
+
+def Login():
+    Username = input("\nUsername: ")
+    Password = pwinput.pwinput()
+    if Username == 'JackAttack612' and Password == 'Madjack612!':
+        print("\nLogin Sucessful")
+        time.sleep(2)
+        main()
+    else:
+        print("Username or Password is incorrect")
+        Login()
+Login()
