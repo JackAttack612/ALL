25330617e92b5a8c9019242989cef1ed0b458d7a
JackAttack612 <jetflyer1130@gmail.com>
2021-09-27 10:10:22 -0400

    Update twitterbot.py

--- Projects/twitterbot.py
+++ Projects/twitterbot.py
@@ -24,6 +24,13 @@ def read():
         time.sleep(3)
         main()
 
+def gread():
+    timeline = api.home_timeline()
+    for tweet in timeline:
+        print(f"{tweet.user.name} said {tweet.text}")
+        time.sleep(3)
+        guest()
+
 def Tweet():
     tweet_input = input("\nWhat do you want to tweet: ")
     api.update_status(tweet_input)
@@ -49,6 +56,26 @@ def user_info():
     except:
         print("\nCould not find user")
         main()
+
+def guser_info():
+    try:
+        user_info_name = input("\nWhat user do you want to find: ")
+        user = api.get_user(user_info_name)
+        followers_count = user.followers_count
+  
+        print("User details:")
+        print('Username: ' + user.name)
+        print('User Description: ' + user.description)
+        print('User Location: ' + user.location)
+        print("Followers: " + str(followers_count))
+
+        print("\nLast 20 Followers:")
+        for follower in user.followers():
+            print(follower.name)
+        guest()
+    except:
+        print("\nCould not find user")
+        guest()
 
 def like_mentioned():
     tweets = api.mentions_timeline()
@@ -91,6 +118,20 @@ def main():
         print("\nDid not receive a valid input")
         main()
 
+def guest():
+    print("type help to get a list of things you can do")
+    start = input("\nWhat do you want to do: ").lower()
+    if start == 'help':
+        print('quit\nread\nuserinfo')
+    elif start == 'read':
+        gread()
+    elif start == 'userinfo':
+        guser_info()
+    elif start == 'quit':
+        import all
+    else:
+        guest()
+
 def Login():
     Username = input("\nUsername: ")
     Password = pwinput.pwinput()
@@ -98,9 +139,15 @@ def Login():
         print("\nLogin Sucessful")
         time.sleep(2)
         main()
+    elif Username == 'Guest' and Password == 'Guest123':
+        print("Login Sucessful")
+        print("Guest Mode: Limited Abilities")
+        time.sleep(3)
+        guest()
     elif Username or Password == 'quit':
         import all
     else:
         print("Username or Password is incorrect")
         Login()
+print("Guest Login:\nUsername: Guest\nPassword: Guest123")
 Login()
