1775b25af79c9f540619e033767ee9a5c5b054ac
JackAttack612 <jetflyer1130@gmail.com>
2021-10-11 19:55:45 -0400

    Update DiceRoller

--- all.py
+++ all.py
@@ -186,55 +186,21 @@ def proj_rps():
 
 def proj_diceroller():
     def select():
-        choice = input("\nHow many sides do you want (6, 12, 24, 36): ").lower()
-        if choice == '6':
-            side6()
-        elif choice == 'quit':
-            print('Loading Menu...')
+        try:
+            choice = input("\nHow many sides do you want: ").lower()
+            if choice == 'quit':
+                projects()
+            else:
+                roll = random.randint(1, int(choice))
+                print("You rolled: " + str(roll))
+                select()
+        except ValueError:
+            print("\n⚠")
+            print("ValueError: You have to input a number")
+            print("⚠")
             time.sleep(2)
-            projects()
-        elif choice == '12':
-            side12()
-        elif choice == '24':
-            side24()
-        elif choice == '36':
-            side36()
-        elif choice == "69":
-            sixnine()
-        elif choice == '420':
-            fourtwenty()
-        else:
-            print("\nPlease Choose 6, 12, 24, or 36")
-            time.sleep(3)
             select()
 
-    def side6():
-        choice6 = random.choice(['1', '2', '3', '4', '5', '6'])
-        print("\nYou rolled a " + choice6)
-        select()
-
-    def side12():
-        choice12 = random.choice(['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'])
-        print("\nYou rolled a " + choice12)
-        select()
-
-    def side24():
-        choice24 = random.choice(['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24'])
-        print("\nYou rolled a " + choice24)
-        select()
-
-    def side36():
-        choice36 = random.choice(['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35', '36'])
-        print("\nYou rolled a " + choice36)
-        select()
-
-    def sixnine():
-        print("\nHAHAHAHAHA, Ur so funny")
-        select()
-
-    def fourtwenty():
-        print("\nSmokin weed everyday, 42000000000000000000000000000000000000")
-        select()
 
     select()
 
